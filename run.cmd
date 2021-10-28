24e6849582b819a0db411c379debe7e252399bc5
AnnYat <AnnYat@BitBucket.com>
2021-10-28 15:24:44 +0300

    Setup project

---
+++ run.cmd
@@ -0,0 +1,2 @@
+call chcp 1251
+call java -jar target/matrix_sort.jar
---
+++ src/main/java/ua/com/matrix/MatrixSortMain.java
@@ -0,0 +1,9 @@
+package ua.com.matrix;
+
+public class MatrixSortMain{
+
+    public static void main(String[] args){
+        System.out.println("Main");
+    }
+
+}
