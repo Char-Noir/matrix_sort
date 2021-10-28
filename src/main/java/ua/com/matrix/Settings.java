21f99fdeb84c3106d12bb491d4b5341c270941d6
AnnYat <AnnYat@BitBucket.com>
2021-10-28 18:02:57 +0300

    Add Settings class

---
+++ src/main/java/ua/com/matrix/Settings.java
@@ -0,0 +1,12 @@
+package ua.com.matrix;
+
+public class Settings {
+
+    public static final int minRandValue = 1;
+    public static final int maxRandValue = 16;
+
+    public static int calcMatrixSize(int n){
+        return (n*10)+1;
+    }
+
+}
